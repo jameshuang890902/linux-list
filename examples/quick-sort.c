e05e73eaedb655ee8050f91bd2f665f6800715ea
黃子峻 <jameshuang890902@gmail.com>
2021-03-04 14:42:07 +0800

    Finish the non recursive quicksort in quick-sort.c
    Add function list_qsort_non_recursive in quick-sort.c.
    This function is the non recursive version of list_qsort fuction.

--- examples/quick-sort.c
+++ examples/quick-sort.c
@@ -1,10 +1,13 @@
 #include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
 #include "common.h"
+#include "list.h"
+//#include "quick-sort_non_recursive.h"
 
-static uint16_t values[256];
+static uint16_t values[256];  // 256
 
 static void list_qsort(struct list_head *head)
 {
@@ -36,6 +39,104 @@ static void list_qsort(struct list_head *head)
     list_splice_tail(&list_greater, head);
 }
 
+typedef struct stack_ele STACK_ELE;
+struct stack_ele {
+    struct list_head temp_head;
+    // bool is_right_child;
+    int merged;  // defualt false
+    int level;
+};
+
+static void list_qsort_non_recursive(struct list_head *head)
+{
+    struct listitem *item = NULL, *is = NULL;
+
+    struct listitem *pivot[256];
+    struct stack_ele stack[512];
+    int stack_top = -1, stack_bottum = -1, stack_current = -1, pivot_top = -1,
+        pivot_bottum = -1, pivot_current = -1;  // 256 as ARRAY_SIZE(values)
+
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    INIT_LIST_HEAD(&stack[0].temp_head);  // list_less
+    INIT_LIST_HEAD(&stack[1].temp_head);  // list_large
+
+    stack[0].level = 1;
+    stack[1].level = 1;
+
+    pivot[0] = list_first_entry(head, struct listitem, list);
+    list_del(&pivot[0]->list);
+
+    list_for_each_entry_safe (item, is, head, list) {
+        if (cmpint(&item->i, &pivot[0]->i) < 0)
+            list_move_tail(&item->list, &stack[0].temp_head);
+        else
+            list_move(&item->list, &stack[1].temp_head);
+    }
+
+    stack_top += 2;
+    stack_bottum++;
+    stack_current++;
+    pivot_top += 1;
+    pivot_bottum++;
+
+    while (stack_top >= stack_current) {
+        if (!list_empty(&stack[stack_current].temp_head) &&
+            !list_is_singular(&stack[stack_current].temp_head)) {
+            stack[stack_current].merged = 1;
+            pivot_top++;
+            pivot[pivot_top] = list_first_entry(&stack[stack_current].temp_head,
+                                                struct listitem, list);
+            list_del(&pivot[pivot_top]->list);
+
+            INIT_LIST_HEAD(&stack[stack_top + 1].temp_head);
+            INIT_LIST_HEAD(&stack[stack_top + 2].temp_head);
+
+            list_for_each_entry_safe (item, is, &stack[stack_current].temp_head,
+                                      list) {
+                if (cmpint(&item->i, &pivot[pivot_top]->i) < 0)
+                    list_move_tail(&item->list,
+                                   &stack[stack_top + 1].temp_head);
+                else
+                    list_move(&item->list, &stack[stack_top + 2].temp_head);
+            }
+            stack[stack_top + 1].level = stack[stack_current].level + 1;
+            stack[stack_top + 2].level = stack[stack_current].level + 1;
+            stack_top += 2;
+        } else
+            stack[stack_current].merged = 0;
+        stack_current++;
+    }
+
+    int less_index, greater_index = stack_top, merge_index;
+    pivot_current = pivot_top;
+
+    while (greater_index > stack_bottum + 1) {
+        less_index = greater_index - 1;
+        merge_index = less_index - 1;
+
+        while (stack[merge_index].level != stack[less_index].level - 1)
+            merge_index--;
+        while (stack[merge_index].merged != 1)
+            merge_index--;
+
+        list_add(&pivot[pivot_current]->list, &stack[merge_index].temp_head);
+        list_splice(&stack[less_index].temp_head,
+                    &stack[merge_index].temp_head);
+        list_splice_tail(&stack[greater_index].temp_head,
+                         &stack[merge_index].temp_head);
+
+        pivot_current--;
+        greater_index = less_index - 1;
+        stack[merge_index].merged = 0;
+    }
+    less_index = greater_index - 1;
+    list_add(&pivot[pivot_current]->list, head);
+    list_splice(&stack[less_index].temp_head, head);
+    list_splice_tail(&stack[greater_index].temp_head, head);
+}
+
 int main(void)
 {
     struct list_head testlist;
@@ -53,20 +154,25 @@ int main(void)
         assert(item);
         item->i = values[i];
         list_add_tail(&item->list, &testlist);
+        printf("%d ", item->i);
     }
+    printf("\n");
 
     assert(!list_empty(&testlist));
 
     qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
-    list_qsort(&testlist);
+    // list_qsort(&testlist);
+    list_qsort_non_recursive(&testlist);
 
     i = 0;
     list_for_each_entry_safe (item, is, &testlist, list) {
         assert(item->i == values[i]);
+        printf("%d ", item->i);
         list_del(&item->list);
         free(item);
         i++;
     }
+    printf("\n");
 
     assert(i == ARRAY_SIZE(values));
     assert(list_empty(&testlist));
